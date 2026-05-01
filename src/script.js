e82ebf03b70acbbafc2d08cf76f3097a3519df36
Daniel Ribeiro <neto.daniribeiro@gmail.com>
2026-05-01 10:55:15 -0300

    fix dev command and refactor code

--- src/script.js
+++ src/script.js
@@ -1,58 +1,70 @@
-function buttonAlert(e) {
+window.onload = () => {
+  const buttonLoginElement = document.getElementById('button-login');
+  const buttonSubmitElement = document.getElementById('facebook-register');
+  const inputs = document.querySelectorAll('#inputs-container input');
+
+  buttonLoginElement.onclick = showAlert;
+  buttonSubmitElement.onclick = onFormSubmit;
+  for (let index = 0; index < inputs.length; index += 1) {
+    inputs[index].onclick = () => showCustomGenderOptions(inputs[index]);
+  }
+};
+
+function showAlert(e) {
   e.preventDefault();
   const emailInputElement = document.getElementById('user-email-phone');
   alert(emailInputElement.value);
 }
 
-function genderCheck() {
-  const genderVerify = document.getElementsByName('gender');
-  for (let index = 0; index < genderVerify.length; index += 1) {
-    if (genderVerify[index].checked) {
-      return genderVerify[index].value;
-    }
-  }
+function onFormSubmit(event) {
+  event.preventDefault();
+  if (!validateForm()) return
+  handleFormSubmit();
 }
 
-function clearRightContent(nameElement, emailElement,
-  birthdateElement, genderElement) {
-  const rightContent = document.querySelector('.right-content');
-  rightContent.innerHTML = '';
-  rightContent.appendChild(nameElement);
-  rightContent.appendChild(emailElement);
-  rightContent.appendChild(birthdateElement);
-  rightContent.appendChild(genderElement);
-}
-
-function submitedForm() {
-  const nameValue = document.getElementById('firstname').value;
-  const lastnameValue = document.getElementById('lastname').value;
-  const email = document.getElementById('email').value;
-  const birthdate = document.getElementById('birthdate').value;
-  const gender = genderCheck();
-  const nameElement = document.createElement('p');
-  nameElement.innerHTML = `"Olá, ${nameValue} ${lastnameValue}`;
-  const emailElement = document.createElement('p');
-  emailElement.innerHTML = email;
-  const birthdateElement = document.createElement('p');
-  birthdateElement.innerHTML = birthdate;
-  const genderElement = document.createElement('p');
-  genderElement.innerHTML = gender;
-  clearRightContent(nameElement, emailElement, birthdateElement, genderElement);
-}
-
-function inputValidate(e) {
-  e.preventDefault();
+function validateForm() {
   const inputs = document.querySelectorAll('.text-class');
   const registerForm = document.querySelector('#register-form');
   for (let index = 0; index < inputs.length; index += 1) {
     if (inputs[index].value === '') {
-      const invalidField = document.createElement('span');
-      invalidField.innerText = 'Campos inválidos';
+      const invalidField = createElement("Campos inválidos", 'span')
       registerForm.appendChild(invalidField);
-      return;
+      return false;
     }
   }
-  submitedForm();
+  return true
+}
+
+function handleFormSubmit() {
+  const nameElement = createElement(`"Olá, ${document.getElementById('firstname').value} ${document.getElementById('lastname').value}`);
+  const emailElement = createElement(document.getElementById('email').value);
+  const birthdateElement = createElement(document.getElementById('birthdate').value);
+  const genderElement = createElement(getGenderValue());
+
+  setRightContent([nameElement, emailElement, birthdateElement, genderElement]);
+}
+
+function getGenderValue() {
+  const genderElements = document.getElementsByName('gender');
+  for (let index = 0; index < genderElements.length; index += 1) {
+    if (genderElements[index].checked) {
+      return genderElements[index].value;
+    }
+  }
+}
+
+function setRightContent(elements) {
+  const rightContent = document.querySelector('.right-content');
+  rightContent.innerHTML = '';
+
+  elements.forEach(element => rightContent.appendChild(element))
+}
+
+
+function createElement(content, element = 'p') {
+  const htmlElement = document.createElement(element);
+  htmlElement.innerHTML = content;
+  return htmlElement;
 }
 
 function showCustomGenderOptions(input) {
@@ -64,13 +76,3 @@ function showCustomGenderOptions(input) {
   }
 }
 
-window.onload = () => {
-  const buttonLoginElement = document.getElementById('button-login');
-  const buttonSubmitElement = document.getElementById('facebook-register');
-  const inputs = document.querySelectorAll('#inputs-container input');
-  buttonLoginElement.onclick = buttonAlert;
-  buttonSubmitElement.onclick = inputValidate;
-  for (let index = 0; index < inputs.length; index += 1) {
-    inputs[index].onclick = () => showCustomGenderOptions(inputs[index]);
-  }
-};
