46614f174743ca984f6888ced6865d9989400375
Daniel Ribeiro <neto.daniribeiro@gmail.com>
2026-05-02 06:45:51 -0300

    add tests with playwright

---
+++ playwright.config.js
@@ -0,0 +1,42 @@
+import { defineConfig, devices } from "@playwright/test";
+
+export default defineConfig({
+  testDir: "./tests",
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: "html",
+  use: {
+    trace: "on-first-retry",
+  },
+
+  projects: [
+    {
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
+    },
+    {
+      name: "firefox",
+      use: { ...devices["Desktop Firefox"] },
+    },
+    {
+      name: "webkit",
+      use: { ...devices["Desktop Safari"] },
+    },
+    {
+      name: "Microsoft Edge",
+      use: { ...devices["Desktop Edge"], channel: "msedge" },
+    },
+    {
+      name: "Google Chrome",
+      use: { ...devices["Desktop Chrome"], channel: "chrome" },
+    },
+  ],
+
+  webServer: {
+    command: "pnpm dev",
+    url: "http://localhost:9000",
+    reuseExistingServer: !process.env.CI,
+  },
+});
---
+++ tests/log-in-page.spec.js
@@ -0,0 +1,47 @@
+import { describe, test } from "@playwright/test";
+
+describe("Login page", () => {
+  test("Login", async ({ page }) => {
+    await page.goto("http://localhost:9000/");
+    await page.getByRole("textbox", { name: "Email ou telefone" }).click();
+    await page
+      .getByRole("textbox", { name: "Email ou telefone" })
+      .fill("mail@mail.com");
+    await page.locator("#user-password").click();
+    await page.locator("#user-password").fill("senha123");
+    page.once("dialog", (dialog) => {
+      console.log(`Dialog message: ${dialog.message()}`);
+      dialog.dismiss().catch(() => {});
+    });
+    await page.getByRole("button", { name: "Entrar" }).click();
+  });
+
+  test("Sign In", async ({ page }) => {
+    await page.goto("http://localhost:9000/");
+    await page.getByRole("textbox", { name: "Email ou telefone" }).click();
+    await page.getByRole("textbox", { name: "Nome", exact: true }).click();
+    await page
+      .getByRole("textbox", { name: "Nome", exact: true })
+      .fill("Meu nome");
+    await page.getByRole("textbox", { name: "Nome", exact: true }).press("Tab");
+    await page
+      .getByRole("textbox", { name: "Sobrenome" })
+      .fill("Meu sobrenome");
+    await page.getByRole("textbox", { name: "Sobrenome" }).press("Tab");
+    await page
+      .getByRole("textbox", { name: "Celular ou email" })
+      .fill("neto.dani@mail.com");
+    await page.getByRole("textbox", { name: "Celular ou email" }).press("Tab");
+    await page.getByRole("textbox", { name: "Nova senha" }).fill("123");
+    await page.getByRole("textbox", { name: "Data de nascimento" }).click();
+    await page
+      .getByRole("textbox", { name: "Data de nascimento" })
+      .fill("12/02/1200");
+    await page.getByText("Feminino Masculino Customizado").click();
+    await page.locator("#custom-gender").check();
+    await page.getByRole("textbox", { name: "Gênero (opcional)" }).click();
+    await page
+      .getByRole("textbox", { name: "Gênero (opcional)" })
+      .fill("genero customizado");
+  });
+});
