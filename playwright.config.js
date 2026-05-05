e2c9f62c84083a18c6e99ce1a245113be189beb0
Daniel Ribeiro <neto.daniribeiro@gmail.com>
2026-05-05 04:40:36 -0300

    chore: change playwright config

--- playwright.config.js
+++ playwright.config.js
@@ -6,27 +6,14 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: "html",
   use: {
     trace: "on-first-retry",
   },
 
   projects: [
     {
-      name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
-    },
-    {
       name: "firefox",
       use: { ...devices["Desktop Firefox"] },
-    },
-    {
-      name: "webkit",
-      use: { ...devices["Desktop Safari"] },
-    },
-    {
-      name: "Microsoft Edge",
-      use: { ...devices["Desktop Edge"], channel: "msedge" },
     },
     {
       name: "Google Chrome",
